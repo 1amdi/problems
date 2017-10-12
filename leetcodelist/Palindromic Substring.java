4f95b942b0adffcb145f051120e761f88c3146c7
1amdi <xiangmiaozi@gmail.com>
2017-10-12 18:13:42 +0800

    Create Palindromic Substring.java

---
+++ leetcodelist/Palindromic Substring.java
@@ -0,0 +1,19 @@
+Problem:
+
+Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+
+Example:
+
+Input: "babad"
+
+Output: "bab"
+
+Note: "aba" is also a valid answer.
+Example:
+
+Input: "cbbd"
+
+Output: "bb"
+
+解析链接：https://segmentfault.com/a/1190000003914228#articleHeader4
+
